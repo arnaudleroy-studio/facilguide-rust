44abc96a9f1c515b744094343de60aa433a3a77b
Arnaud Leroy <dropthehq@gmail.com>
2026-03-29 14:16:23 +0100

    Add .gitignore, remove build artifacts from tracking

--- target/debug/deps/facilguide-ac83eb8ae8b93119.d
+++
@@ -1,5 +0,0 @@
-/private/tmp/rust-rewrite/facilguide/target/debug/deps/facilguide-ac83eb8ae8b93119.d: src/lib.rs
-
-/private/tmp/rust-rewrite/facilguide/target/debug/deps/facilguide-ac83eb8ae8b93119: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/facilguide-e4cf7487a710c2f7.d
+++
@@ -1,7 +0,0 @@
-/private/tmp/rust-rewrite/facilguide/target/debug/deps/facilguide-e4cf7487a710c2f7.d: src/lib.rs
-
-/private/tmp/rust-rewrite/facilguide/target/debug/deps/libfacilguide-e4cf7487a710c2f7.rlib: src/lib.rs
-
-/private/tmp/rust-rewrite/facilguide/target/debug/deps/libfacilguide-e4cf7487a710c2f7.rmeta: src/lib.rs
-
-src/lib.rs:
